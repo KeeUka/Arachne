8707e7162ac8befad9e33042e737aabd5e2a3d9c
Kee <kee@glazziq.com>
2016-10-26 12:32:23 +0700

    Checkpoint

---
+++ src/net/keeratipong/arachne/crawler/Crawler.java
@@ -0,0 +1,53 @@
+package net.keeratipong.arachne.crawler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class Crawler extends WebCrawler {
+
+	public static final String EMAIL_PATTERN = "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+";
+	
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		return whitelisted(href) && href.startsWith(CrawlerState.getInstance().getDomain());
+	}
+	
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		CrawlerState.getInstance().setCurrentUrl(url);
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String html = htmlParseData.getHtml();
+			List<String> emails = extractEmails(html.toString());
+			for (int i = 0; i < emails.size(); i++) {
+				CrawlerState.getInstance().setLastEmailFound(emails.get(i));
+			}
+		}
+	}
+	
+	private boolean whitelisted(String href) {
+		String sPattern = ".*(\\.(css|js|gif|jpg" + "|png|mp3|mp3|zip|gz))$";
+		Pattern pattern = Pattern.compile(sPattern);
+		return !pattern.matcher(href).matches();
+	}
+
+	public static List<String> extractEmails(String source) {
+		List<String> emails = new ArrayList<String>();
+		Matcher m = Pattern.compile(EMAIL_PATTERN).matcher(source);
+		while (m.find()) {
+			emails.add(m.group());
+		}
+		return emails;
+	}
+	
+}
--- src/net/keeratipong/arachne/crawler/CrawlerState.java
+++ src/net/keeratipong/arachne/crawler/CrawlerState.java
@@ -1,11 +1,15 @@
 package net.keeratipong.arachne.crawler;
 
-public class CrawlerState {
+import java.util.Observable;
+
+public class CrawlerState extends Observable {
 
 	private static CrawlerState instance;
 
 	private String domain;
-
+	private String currentUrl;
+	private String lastEmailFound;
+	
 	public static CrawlerState getInstance() {
 		if (instance == null) {
 			instance = new CrawlerState();
@@ -23,5 +27,21 @@ public class CrawlerState {
 	public String getDomain() {
 		return domain;
 	}
-
+	
+	public String getCurrentUrl() {
+		return currentUrl;
+	}
+	
+	public void setCurrentUrl(String currentUrl) {
+		this.currentUrl = currentUrl;
+	}
+	
+	public void setLastEmailFound(String lastEmailFound) {
+		this.lastEmailFound = lastEmailFound;
+	}
+	
+	public String getLastEmailFound() {
+		return lastEmailFound;
+	}
+	
 }
